9126518d6d638845b964750a0c34a7399d43efe5
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-09-27 22:18:08 -0500

    setup fix

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 
 version = '0.1'
 
-setup(name='django-moderation-queue',
+setup(name='django-activity-log',
       version=version,
-      description="Generic Django objects moderation application. With moderation queue!",
+      description="Generic Django activity logging app!",
 
       classifiers=[
         'Development Status :: 1 - Initial',
@@ -16,10 +16,10 @@ setup(name='django-moderation-queue',
         'Programming Language :: Python',
         'Framework :: Django',
       ],
-      keywords='django moderation models',
+      keywords='django activity models',
       author='Arcady Chumachenko',
       author_email='arcady.chumachenko@gmail.com',
-      url='http://github.com/ilvar/django-moderation-queue',
+      url='http://github.com/ilvar/django-activity-log',
       license='BSD',
       packages = find_packages('.'),
       package_dir = {'': '.'},
