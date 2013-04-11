babcb78bf8ea092a6d0ab7a59d47091fe9f25f6e
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-04-11 16:24:22 +0400

    filter views

--- activity/admin.py
+++ activity/admin.py
@@ -5,13 +5,7 @@ from activity.models import Activity, ACTION_VIEWED
 
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ['content_object', 'user', 'action', 'timestamp']
-
-    def queryset(self, request):
-        qs = Activity.objects.exclude(action=ACTION_VIEWED)
-        ordering = self.get_ordering(request)
-        if ordering:
-            qs = qs.order_by(*ordering)
-        return qs
+    list_filter = ['action']
 
 admin.site.register(Activity, ActivityAdmin)
 
