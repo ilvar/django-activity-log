57819a4dfaed485d159e9767750152c701823890
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-04-11 16:12:44 +0400

    filter views

--- activity/admin.py
+++ activity/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from activity.models import Activity
+from activity.models import Activity, ACTION_VIEWED
+
 
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ['content_object', 'user', 'action', 'timestamp']
+
+    def queryset(self, request):
+        return Activity.objects.exclude(action=ACTION_VIEWED)
 
 admin.site.register(Activity, ActivityAdmin)
 
